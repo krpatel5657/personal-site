eeb0ae0b3a7242bb026d4d9db551d9e7d37e0ab3
Krishna Patel <krpatel5657@gmail.com>
2022-02-13 22:09:52 -0500

    updated header

--- src/Title.js
+++ src/Title.js
@@ -4,12 +4,14 @@ import headshot from "./img/Krishna-Headshot.JPG"
 
 function Title() {
     return (
-        <div class="back-pic">
-        <div class="header-main">
-            <img class="clip-polygon" id="headshot" src={headshot} alt="headshot"/>
-            <div id="title-info">
-                <h1 id="name-title">KRISHNA<br />PATEL</h1>
-                <p>SITE CURRENTLY UNDER CONSTRUCTION</p>
+        <div class="back-pic vignette-shadow">
+            <div class="border-back">
+                <div class="header-main">
+                    <img class="clip-polygon vignette-shadow" id="headshot" src={headshot} alt="headshot"/>
+                    <div id="title-info">
+                        <h1 id="name-title">KRISHNA<br />PATEL</h1>
+                        <p>SITE CURRENTLY UNDER CONSTRUCTION</p>
+                    </div>
             </div>
         </div>
             <div class="arrow-down"></div>
